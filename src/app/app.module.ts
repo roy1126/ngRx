373cb1d29643d08efb22b8bb6ace1bee736fe61d
Roy Abanilla <royabanilla@gmail.com>
2021-08-05 13:59:48 +0800

    added files

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
+import * as fromApp from "./store/app.reducer";
+import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "src/environments/environment";
+import { RecipeEffects } from "./recipes/store/recipe.effects";
+// import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([AuthEffects, RecipeEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    // StoreRouterConnectingModule.forRoot({}),
+    SharedModule,
+    CoreModule,
+  ],
+  bootstrap: [AppComponent],
+  // providers: [LoggingService]
+})
+export class AppModule {}
---
+++ src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from "@angular/router";
+
+import { Recipe } from "./recipe.model";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import * as RecipeActions from "../recipes/store/recipe.actions";
+import { Actions, ofType } from "@ngrx/effects";
+import { map, switchMap, take } from "rxjs/operators";
+import { of } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(
+    private store: Store<fromApp.AppState>,
+    private actions$: Actions
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.store.select("recipes").pipe(
+      take(1),
+      map((recipesState) => {
+        return recipesState.recipes;
+      }),
+      switchMap((recipes) => {
+        if (recipes.length === 0) {
+          this.store.dispatch(new RecipeActions.FetchRecipes());
+          return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
+        } else {
+          return of(recipes);
+        }
+      })
+    );
+  }
+}
